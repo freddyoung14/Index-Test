82a93f67c9c0294488b5a0a63fceae7d1c00231f
freddyoung14 <freddyyoung1893@yahoo.com.au>
2016-04-02 03:40:29 +1000

    started pseudo code

--- FrederickYoungA1.py
+++ FrederickYoungA1.py
@@ -12,12 +12,79 @@ Pseudocode:
 function main()
 	display welcome message
 	display menu
+	get user choice
+
+	while choice is not 'Q'
+	    if choice is 'L'
+	        display all items
+
 	...
 	... <-- [finish after all other functions are complete.]
 
 function loading_items()
+    import csv
 
-function hiring_item()
+
+
+
+
+
+
+function hiring_item():
+
+
+
+
+
+"""
+
+
+
+
+Menu = '\nMENU: \n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new item to stock\n(Q)uit'
+
+def main():
+
+
+    print('\n\nWelcome to the CP1404 Item Hiring Program!')
+    print('written by Frederick Michael Young, March 2016')
+
+    print(Menu)
+
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- practice.py
+++ practice.py
@@ -1,22 +1,12 @@
-# .py file used for testing/practicing code
-
-
-
-Menu = '\nMENU: \n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new item to stock\n(Q)uit'
-
-print(Menu)
-print('\n')
-
+# .py file for practicing/testing code.
 
 INPUT_FILE = "items.csv"
 
-items = open(INPUT_FILE, "r")
+in_file = open(INPUT_FILE, "r")
 
-print(items.read())
+print(in_file.read())
 
-items.close()
-
-
+in_file.close()
 
 
 
