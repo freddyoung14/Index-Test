fd0d7973c8ad17096cbc2fc975ab70f2468bfcf0
freddyoung14 <freddyyoung1893@yahoo.com.au>
2016-04-06 11:51:23 +1000

    add_items() function complete.

--- FrederickYoungA1.py
+++ FrederickYoungA1.py
@@ -25,6 +25,18 @@ def main():
     while choice != 'Q':
         if choice == 'L':
             list_items()
+        elif choice == 'H':
+            hire_item()
+        elif choice == 'R':
+            return_item()
+        elif choice == 'A':
+            add_items()
+        else:
+            print('Invalid menu choice')
+        print(Menu)
+        choice = input('>>> ').upper()
+
+    print('Have a nice day :)')
 
 
 
@@ -47,6 +59,30 @@ def loading_items():
     in_file.close()
 
 
+def list_items():
+    print('All items on file (* indicates item is currently out):')
+
+    INPUT_FILE = "items.csv"
+
+    in_file = open(INPUT_FILE, "r")
+    item_list = in_file.readlines()
+
+    mydictionary = {'ColumnName1':0, 'ColumnName2':1, 'ColumnName3':2, 'ColumnName4':3}
+    # space = ''.center(4).ljust(20).rjust(6) --------- testing alignment
+
+    count = -1
+
+    for row in item_list:
+        row = row.rstrip()
+        mydictionary = row.split(',')
+        count += 1
+
+        print('{0} - {1} ({2}) = ${3:.2f}'.format(count, mydictionary[0], mydictionary[1], (float(mydictionary[2]))),
+              (mydictionary[3].replace('out', '*').replace('in', '')).strip())
+
+
+
+    in_file.close()
 
 
 def add_items():
