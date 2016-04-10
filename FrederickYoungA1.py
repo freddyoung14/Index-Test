80be02153538ac7e4565df65d33864cd5bd34884
freddyoung14 <freddyyoung1893@yahoo.com.au>
2016-04-11 08:02:34 +1000

    typed pseudocode from written format in textbook into pycharm -loadingitems() -hireitems()

--- FrederickYoungA1.py
+++ FrederickYoungA1.py
@@ -6,7 +6,7 @@
 
 	GITHUB LINK - ....................
 
-Pseudocode: (ONLY FOR loading_items & hiring_items) !!
+
 
 
 """
@@ -26,7 +26,7 @@ def main():
         if choice == 'L':
             list_items()
         elif choice == 'H':
-            hire_item()
+            hire_items()
         elif choice == 'R':
             return_item()
         elif choice == 'A':
@@ -41,9 +41,23 @@ def main():
 
 
 
+"""
+Pseudocode for loading items
+.....................................
 
+function loading_items()
+    INPUT_FILE = "items.csv
 
+    open "items.csv as in_file for reading
+    readlines in in_file as item_list
+    num_items = 0
 
+    for row in item_list:
+        words_list = row split string
+        num_items = num_items + 1
+    display num_items & message to user
+    close in_file
+"""
 def loading_items():
     INPUT_FILE = "items.csv"
 
@@ -130,6 +144,211 @@ def add_items():
     print('{0} ({1}), ${2:.2f} now available for hire'.format(item_name, item_description, item_cost))
 
     OUTPUT_FILE.close()
+
+"""
+Pseudocode for hiring an item
+.....................................
+
+function loading_items()
+    INPUT_FILE = "items.csv
+
+    open "items.csv" as in_file for reading
+    readlines in in_file as item_list
+
+    mydictionary = empty map
+    count = 0
+
+    no_items = no items message
+
+    for line in item_list
+        row = row strip whitespace
+        mydictionary = row split string
+        set item: name, description, cost & stock value to mydictionary index'
+        replace 'out' with * & replace 'in' with empty string
+        count = count + 1
+
+    if row ends with 'in'
+        display mydictionary
+    else if row doesn't end with 'in' and ends with 'out'
+        display no items message
+    close in_file
+
+    OUTPUT_FILE = 'items.csv'
+
+    open "items.csv" as out_file for editing and reading
+    readlings in out_file as items_list_out
+
+    display enter number of an item message
+
+    valid_input = False
+    repeat while not valid_input
+        try
+            get item_num
+            if item_num < 0
+                display invalid number
+            else
+                valid_input = True
+        except ValueError
+            display invalid input message
+
+    for i, row in enumerate (item_list_out, 1)
+        if i == item_num
+            stop loop(break)
+
+    hired_item = row.replace string, 'in' with 'out'
+
+    initially seek 0 at out_file
+    for line in item_list_out:
+        if 1 != i:
+            write (i) to out_file
+    write (hired_item) to out_file
+
+    hired_item = list(hired_item) split string
+
+    display item name, hire message and item cost
+
+    close out_file
+"""
+def hire_items():
+
+    INPUT_FILE = "items.csv"
+
+    in_file = open(INPUT_FILE, "r")
+
+    item_list = in_file.readlines()
+
+    mydictionary = {'ItemName':0, 'ItemDescription':1, 'ItemCost':2, 'ItemStockValue':3}
+
+    count = 0
+
+    no_items = 'No items are currently on hire'
+
+    for row in item_list:
+
+            row = row.rstrip()
+            mydictionary = row.split(',')
+            item_name = mydictionary[0]
+            item_description = mydictionary[1]
+            item_cost = float(mydictionary[2])
+            item_stock_value = mydictionary[3].replace('out', ' *').replace('in', ' ')
+            count += 1
+
+            if row.endswith('in'):
+
+                print('{} - {} ({}) = ${:.2f}{}'.format(count, item_name, item_description, item_cost,
+                                                         item_stock_value))
+
+            elif 'in' not in row and 'out' not in row:
+                print(no_items)
+    in_file.close()
+
+    OUTPUT_FILE = 'items.csv'
+    out_file = open(OUTPUT_FILE, "r+")
+
+    item_list_out = out_file.readlines()
+
+    print('Enter the number of an item to hire')
+
+
+    valid_input = False
+    while not valid_input:
+        try:
+            item_num = int(input('>>> '))
+            if item_num < 0:
+                print('Invalid number')
+            else:
+                valid_input = True
+        except ValueError:
+            print("Invalid Input/ enter a  number")
+
+
+    for i, row in enumerate (item_list_out, 1):
+        if i == item_num:
+            break
+
+    hired_item = (row.replace('in', 'out'))
+
+    out_file.seek(0)
+    for line in item_list_out:
+        if 1 != i:
+            out_file.write(i)
+    out_file.write(hired_item)
+
+    hired_item = list(hired_item.split())
+
+    print(hired_item[0] in row, 'hired for', hired_item[5])
+
+    out_file.close()
+
+def return_item():
+    INPUT_FILE = "items.csv"
+
+    in_file = open(INPUT_FILE, "r")
+
+    item_list = in_file.readlines()
+
+    mydictionary = {'ItemName':0, 'ItemDescription':1, 'ItemCost':2, 'ItemStockValue':3}
+
+
+    count = 0
+
+    no_items = 'No items are currently on hire'
+
+    for row in item_list:
+        row = row.rstrip()
+        mydictionary = row.split(',')
+        item_name = mydictionary[0]
+        item_description = mydictionary[1]
+        item_cost = float(mydictionary[2])
+        item_stock_value = mydictionary[3].replace('out', ' *').replace('in', ' ')
+        count += 1
+
+        if row.endswith('out'):
+
+            print('{} - {} ({}) = ${:.2f}{}'.format(count, item_name, item_description, item_cost,
+                                                         item_stock_value))
+
+        elif 'in' not in row and 'out' not in row:
+            print(no_items)
+    in_file.close()
+
+    OUTPUT_FILE = 'items.csv'
+    out_file = open(OUTPUT_FILE, "r+")
+
+    item_list_out = out_file.readlines()
+
+    print('Enter the number of an item to hire')
+
+
+    valid_input = False
+    while not valid_input:
+        try:
+            item_num = int(input('>>> '))
+            if item_num < 0:
+                print('Invalid number')
+            else:
+                valid_input = True
+        except ValueError:
+            print("Invalid Input/ enter a  number")
+
+
+    for i, row in enumerate (item_list_out, 1):
+        if i == item_num:
+            break
+
+    return_item = (row.replace('out', 'in'))
+
+    out_file.seek(0)
+    for line in item_list_out:
+        if 1 != i:
+            out_file.write(i)
+    out_file.write(return_item)
+
+    hired_item = list(return_item.split())
+
+    print(hired_item[0] in row, 'hired for', hired_item[5])
+
+    out_file.close()
 
 
 
--- practice2.py
+++ practice2.py
@@ -6,7 +6,7 @@
 
 
 def hire_items():
-
+    """
     INPUT_FILE = "items.csv"
 
     in_file = open(INPUT_FILE, "r")
@@ -22,23 +22,28 @@ def hire_items():
     no_items = 'No items are currently on hire'
 
     for row in item_list:
-        row = row.rstrip()
-        mydictionary = row.split(',')
-        item_name = mydictionary[0]
-        item_description = mydictionary[1]
-        item_cost = float(mydictionary[2])
-        item_stock_value = mydictionary[3].replace('out', ' *').replace('in', ' ')
-        count += 1
 
-        if row.endswith('in'):
-            print('{} - {} ({}) = ${:.2f}{}'.format(count, item_name, item_description, item_cost,
-                                                     item_stock_value))
+            row = row.rstrip()
+            mydictionary = row.split(',')
+            item_name = mydictionary[0]
+            item_description = mydictionary[1]
+            item_cost = float(mydictionary[2])
+            item_stock_value = mydictionary[3].replace('out', ' *').replace('in', ' ')
+            count += 1
 
-        elif 'in' not in row and 'out' not in row:
-            print(no_items)
+            if row.endswith('in'):
+
+                print('{} - {} ({}) = ${:.2f}{}'.format(count, item_name, item_description, item_cost,
+                                                         item_stock_value))
+
+            elif 'in' not in row and 'out' not in row:
+                print(no_items)
     in_file.close()
 
-    in_file1 = open(INPUT_FILE, "r+")
+    OUTPUT_FILE = 'items.csv'
+    out_file = open(OUTPUT_FILE, "r+")
+
+    item_list_out = out_file.readlines()
 
     print('Enter the number of an item to hire')
 
@@ -54,31 +59,102 @@ def hire_items():
         except ValueError:
             print("Invalid Input/ enter a  number")
 
-    count = -1
 
-    for i, row in enumerate(item_list, 1):
+    for i, row in enumerate (item_list_out, 1):
         if i == item_num:
             break
-    print(row)
 
+    hired_item = (row.replace('in', 'out'))
 
-    print(item_name in row, 'hired for', item_cost)
+    out_file.seek(0)
+    for line in item_list_out:
+        if 1 != i:
+            out_file.write(i)
+    out_file.write(hired_item)
 
+    hired_item = list(hired_item.split())
 
+    print(hired_item[0] in row, 'hired for', hired_item[5])
 
-   # for row in item_list:
-      #  for_hire = ','.join(list(row.splitlines()))
-      #  print(for_hire)
-
-    in_file1.close()
-
-
-
+    out_file.close()
 
 
 hire_items()
 
 
 
+"""
+def return_item():
+    INPUT_FILE = "items.csv"
 
+    in_file = open(INPUT_FILE, "r")
+
+    item_list = in_file.readlines()
+
+    mydictionary = {'ItemName':0, 'ItemDescription':1, 'ItemCost':2, 'ItemStockValue':3}
+    # space = '' <--- variable for 'neat' alignment
+    # list.split as alternative
+
+    count = 0
+
+    no_items = 'No items are currently on hire'
+
+    for row in item_list:
+        try:
+            row = row.rstrip()
+            mydictionary = row.split(',')
+            item_name = mydictionary[0]
+            item_description = mydictionary[1]
+            item_cost = float(mydictionary[2])
+            item_stock_value = mydictionary[3].replace('out', ' *').replace('in', ' ')
+            count += 1
+        except:
+            if row.endswith('out'):
+
+                print('{} - {} ({}) = ${:.2f}{}'.format(count, item_name, item_description, item_cost,
+                                                         item_stock_value))
+
+            elif 'in' not in row and 'out' not in row:
+                print(no_items)
+    in_file.close()
+
+    OUTPUT_FILE = 'items.csv'
+    out_file = open(OUTPUT_FILE, "r+")
+
+    item_list_out = out_file.readlines()
+
+    print('Enter the number of an item to hire')
+
+
+    valid_input = False
+    while not valid_input:
+        try:
+            item_num = int(input('>>> '))
+            if item_num < 0:
+                print('Invalid number')
+            else:
+                valid_input = True
+        except ValueError:
+            print("Invalid Input/ enter a  number")
+
+
+    for i, row in enumerate (item_list_out, 1):
+        if i == item_num:
+            break
+
+    return_item = (row.replace('out', 'in'))
+
+    out_file.seek(0)
+    for line in item_list_out:
+        if 1 != i:
+            out_file.write(i)
+    out_file.write(return_item)
+
+    hired_item = list(return_item.split())
+
+    print(hired_item[0] in row, 'hired for', hired_item[5])
+
+    out_file.close()
+return_item()
+"""
 
